9c7a73edcce381545f3dd806467345d14b9806d3
JulianPerez852 <jdph852@gmail.com>
2019-06-10 20:20:02 -0500

    commit para evaluacion

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      contador: 0,
+    }
+    this.onClickButton = this.onClickButton.bind(this)
+  }
+
+  onClickButton(){
+    const conteo = this.state.contador;
+    this.setState({
+      contador: conteo + 1
+    })
+  }
+
   render() {
+    const { contador }= this.state;
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{contador}</span>
+        <button id="inc" onClick={this.onClickButton}>Incrementa</button>
       </div>
     );
   }
